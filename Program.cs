b576a339f682ae088c75ef41abf87b84ab037e2d
divkasyahba <divkasyahba@students.amikom.ac.id>
2021-03-18 20:36:49 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Class_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi = new taxi();
+
+            taxi.DriverName = "tukiman";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 77;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOfPassanger();
+
+            Console.ReadKey();
+        }
+}
---
+++ Taxi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_object
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : yes", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("On Duty : no", OnDuty);
+            }
+            Console.WriteLine("Number of Passanger : {0}", NumPassanger);
+
+        }
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOfPassanger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+
+
+
+        }
+    }
+  
+  
+    }
+    
